e82e340a9f801feadce54132e67b47d50ffe40b5
foukan <kaptandewam@hotmail.com>
2021-03-19 21:11:06 +0300

    deploy

---
+++ src/components/Pokemon.vue
@@ -0,0 +1,77 @@
+<template>
+  <div class="container">
+    <h1>Pokemon's</h1>
+    <PokemonSearch 
+      :apiUrl="apiUrl" 
+      @setPokemonUrl="setPokemonUrl" />
+    <PokemonList 
+      :imageUrl="imageUrl" 
+      :apiUrl="apiUrl"
+      @setPokemonUrl="setPokemonUrl" />
+    <PokemonDetail 
+      v-if="showDetail"
+      :pokemonUrl="pokemonUrl"
+      :imageUrl="imageUrl"
+      @closeDetail="closeDetail" />
+  </div>
+</template>
+
+<script>
+  import PokemonSearch from './PokemonSearch.vue';
+  import PokemonList from './PokemonList.vue';
+  import PokemonDetail from './PokemonDetail.vue';
+
+  export default {
+    data: () => {
+      return {
+        imageUrl: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/',
+        apiUrl: 'https://pokeapi.co/api/v2/pokemon/',
+        pokemonUrl: '',
+        showDetail: false
+      }
+    },
+    components: {
+      PokemonSearch,
+      PokemonList,
+      PokemonDetail
+    },
+    methods: {
+      setPokemonUrl(url) {
+        this.pokemonUrl = url;
+        this.showDetail = true;
+      },
+      closeDetail() {
+        this.pokemonUrl = '';
+        this.showDetail = false;
+      }
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+  @import url('https://fonts.googleapis.com/css2?family=Baloo+2:wght@500&family=DotGothic16&family=Orbitron&family=Press+Start+2P&display=swap');
+
+  .container {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    padding: 10px;
+    width: calc(100% - 20px);
+    min-height: calc(100vh - 20px);
+    background-image: url('https://wallpapercave.com/wp/wp1864742.jpg');
+    background-repeat: no-repeat;
+    background-attachment: fixed;
+    background-position: center;
+    background-size: cover;
+
+
+    font-family: 'Baloo 2', cursive;
+    -font-family: 'DotGothic16', sans-serif;
+    font-size: 1rem;
+    font-weight: normal;
+    
+  }
+
+  h1 { color: #efefef; }
+</style>
